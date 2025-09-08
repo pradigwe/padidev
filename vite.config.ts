19cb8e04140e12297b431fdb3fcde6fe9e9c3cae
Precious Adigwe <padigwe@charlotte.edu>
2025-09-08 16:12:27 -0400

    added svgr plugin for .svg compatibility as React Components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+ // vite.config.ts
+    import svgrPlugin from 'vite-plugin-svgr';
+
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgrPlugin(),
+  ],
 })
+
