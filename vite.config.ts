fcac25796d52ff58f2f5851bb1e34588e6c10a9c
Precious Adigwe <padigwe@charlotte.edu>
2025-09-15 18:05:20 -0400

    fixing github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
  // vite.config.ts
-    import svgrPlugin from 'vite-plugin-svgr';
+import svgrPlugin from 'vite-plugin-svgr';
 
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     svgrPlugin(),
