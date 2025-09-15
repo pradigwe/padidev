3b799eb4170bb270251cc5ff486bc37bfa3ca7b1
Precious Adigwe <padigwe@charlotte.edu>
2025-09-15 16:39:59 -0400

    Revert "bug fixes: github pages deployment"
    This reverts commit 64334078ee4d81b974070032f4b60bef9818e8af.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
  // vite.config.ts
-import svgrPlugin from 'vite-plugin-svgr';
+    import svgrPlugin from 'vite-plugin-svgr';
 
 
 // https://vite.dev/config/
