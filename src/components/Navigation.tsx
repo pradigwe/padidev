35d8637cb55e6af36bf33b5cca4f23ab79c36e04
Precious Adigwe <padigwe@charlotte.edu>
2025-09-15 20:20:50 -0400

    updating image structure
    This reverts commit 65e2f5864cea8171cf65af187ffc4dc6935f9d0e.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
-import HomeButton from "../assets/project-images/home-button.svg?react";
-import ProjectButton from "../assets/project-images/projects-button.svg?react";
-import ToolsButton from "../assets/project-images/tools-button.svg?react";
+import HomeButton from "../assets/home-button.svg?react";
+import ProjectButton from "../assets/projects-button.svg?react";
+import ToolsButton from "../assets/tools-button.svg?react";
 
 function Navigation() {
   return (
