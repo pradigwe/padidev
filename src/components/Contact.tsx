ae508b31d3bc598f384b31e6cf061f7a2578ee14
Precious Adigwe <padigwe@charlotte.edu>
2025-09-30 18:58:14 -0400

    create contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,14 @@
 function Contact() {
   return (
-    <>
-      <p>Contact me!</p>
-    </>
+    <section>
+      <h3>Get in Touch</h3>
+      <p>I'd love to connect with you!</p>
+      <p>Email: padigwe06@gmail.com</p>
+      <div>
+        <a href="https://github.com/pradigwe/" target="_blank">GitHub</a>
+        <a href="https://www.linkedin.com/in/pradigwe/" target="_blank">LinkedIn</a>
+      </div>
+    </section>
   );
 }
 
