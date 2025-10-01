e58a80337ce68b14184203e99fd70eeafeccbaa8
Precious Adigwe <padigwe@charlotte.edu>
2025-10-01 18:11:44 -0400

    installing tailwind css, adding prettier plugin for tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
  // vite.config.ts
 import svgrPlugin from 'vite-plugin-svgr';
@@ -11,6 +12,7 @@ export default defineConfig({
   plugins: [
     react(),
     svgrPlugin(),
+    tailwindcss(),
   ],
 })
 
