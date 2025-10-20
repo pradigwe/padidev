39d14e78c0351f08b06290489204db340d4bd48b
Precious Adigwe <padigwe@charlotte.edu>
2025-10-20 02:36:42 -0400

    creating and styling technologies page & tech modal

--- src/components/technologies/TechModal.tsx
+++ src/components/technologies/TechModal.tsx
@@ -1,33 +1,54 @@
-import { projectList } from "../projects/ProjectList";
 import type { Tech } from "./Technologies";
 import MinimizeIcon from "../../assets/icons8-minimize-svgrepo-com.svg?react";
+import { useEffect } from "react";
+import type { Project } from "../projects/Projects";
 
 interface TechModalProps {
   isModalOpen: boolean;
   technology: Tech | null;
   onClose: () => void;
+  techProjects: Project[];
 }
 
-function TechModal({ isModalOpen, technology, onClose }: TechModalProps) {
-  if (!technology || !isModalOpen || !technology.projectDropdowns) return null;
+function TechModal({
+  isModalOpen,
+  technology,
+  onClose,
+  techProjects,
+}: TechModalProps) {
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [isModalOpen]);
+  if (!technology || !isModalOpen || techProjects.length == 0) return null;
   return (
-    <div>
-      <h3>{technology.name}</h3>
-      <h4>
-        <span className="strong">Category: </span>
-        {technology.category}
-      </h4>
-      <ul>
-        {projectList
-          .filter((project) =>
-            project.techStack.some((tech) => tech === technology.name),
-          )
-          .map((project) => (
-            <li key={project.id}>{project.name}</li>
+    <>
+      <div className="fixed inset-0 z-0 h-dvh w-dvw bg-black/25"></div>
+      <div className="bg-primary border-neutral2 fixed inset-0 z-10 m-auto flex h-4/5 w-4/5 flex-col justify-stretch border-4 p-10 pt-18 text-center">
+        <h3 className="pb-4">{technology.name}</h3>
+        <h4>
+          <span className="font-bold">Category: </span>
+          {technology.category}
+        </h4>
+        <ul className="flex grow flex-col justify-evenly">
+          {techProjects.map((project) => (
+            <li
+              key={project.id}
+              className="bg-neutral1 text-accent p-3 font-bold"
+            >
+              {project.name}
+            </li>
           ))}
-      </ul>
-      <MinimizeIcon onClick={onClose} />
-    </div>
+        </ul>
+        <MinimizeIcon
+          onClick={onClose}
+          className="absolute top-0 right-0 h-16 w-16"
+        />
+      </div>
+    </>
   );
 }
 
--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,7 +1,8 @@
 import techStack from "../../data/techStack.json";
 import { useState } from "react";
 import TechModal from "./TechModal";
-
+import { projectList } from "../projects/ProjectList";
+import type { Project } from "../projects/Projects";
 export interface Tech {
   id: number;
   name: string;
@@ -17,87 +18,97 @@ function Technologies() {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTech, setSelectedTech] = useState<Tech | null>(null);
+  const [techProjects, setTechProjects] = useState<Project[]>([]);
+
   const openModal = (technology: Tech) => {
     setIsModalOpen(true);
     setSelectedTech(technology);
+    setTechProjects(
+      projectList.filter((project) =>
+        project.techStack.includes(technology.name),
+      ),
+    );
   };
   const closeModal = () => {
     setIsModalOpen(false);
+    setTechProjects([]);
   };
 
   return (
     <section className="flex h-dvh flex-col items-center justify-center">
       <h2 className="mb-10 w-4/5">Technologies & Tools</h2>
-      <ul className="flex gap-3 text-lg">
-        <li
-          className="tech-category all selected-category"
-          onClick={() => {
-            setCurrentCategory("all");
-          }}
-        >
-          All
-        </li>
-        <li
-          className="tech-category frameworks"
-          onClick={() => {
-            setCurrentCategory("frameworks");
-          }}
-        >
-          Frameworks
-        </li>
-        <li
-          className="tech-category technologies"
-          onClick={() => {
-            setCurrentCategory("technologies");
-          }}
-        >
-          Technologies
-        </li>
-        <li
-          className="tech-category tools"
-          onClick={() => {
-            setCurrentCategory("tools");
-          }}
-        >
-          Tools
-        </li>
-      </ul>
-      <div className="divider"></div>
-      <ul className="mt-5 flex max-w-xl flex-wrap justify-center gap-x-2 gap-y-3">
-        {techs
-          .filter(
-            (tech) =>
-              currentCategory === "all" || tech.category === currentCategory,
-          )
-          .sort((a, b) => a.category.localeCompare(b.category))
-          .map((tech) => (
-            <div className={`tech-card`}>
-              <img
-                className="w-7"
-                src={
-                  new URL(
-                    `../../assets/tech-images/${tech.icon}?react`,
-                    import.meta.url,
-                  ).href
-                }
-                alt={`${tech.name} icon`}
-              />
-              <li
-                key={tech.id}
+      <div className="basis-1/2">
+        <ul className="flex gap-3 text-lg">
+          <li
+            className="tech-category all selected-category"
+            onClick={() => {
+              setCurrentCategory("all");
+            }}
+          >
+            All
+          </li>
+          <li
+            className="tech-category frameworks"
+            onClick={() => {
+              setCurrentCategory("frameworks");
+            }}
+          >
+            Frameworks
+          </li>
+          <li
+            className="tech-category technologies"
+            onClick={() => {
+              setCurrentCategory("technologies");
+            }}
+          >
+            Technologies
+          </li>
+          <li
+            className="tech-category tools"
+            onClick={() => {
+              setCurrentCategory("tools");
+            }}
+          >
+            Tools
+          </li>
+        </ul>
+        <div className="divider"></div>
+        <ul className="mt-5 flex max-w-xl flex-wrap justify-center gap-x-2 gap-y-3">
+          {techs
+            .filter(
+              (tech) =>
+                currentCategory === "all" || tech.category === currentCategory,
+            )
+            .sort((a, b) => a.category.localeCompare(b.category))
+            .map((tech) => (
+              <div
+                className={`tech-card`}
                 onClick={() => {
                   openModal(tech);
                 }}
               >
-                {tech.name}
-              </li>
-            </div>
-          ))}
-      </ul>
+                <img
+                  className="w-7"
+                  src={
+                    new URL(
+                      `../../assets/tech-images/${tech.icon}?react`,
+                      import.meta.url,
+                    ).href
+                  }
+                  alt={`${tech.name} icon`}
+                />
+                <li key={tech.id}>{tech.name}</li>
+              </div>
+            ))}
+        </ul>
+      </div>
+
       {
         <TechModal
           isModalOpen={isModalOpen}
           technology={selectedTech}
           onClose={closeModal}
+          techProjects={techProjects}
         />
       }
     </section>
