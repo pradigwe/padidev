371376f7d6885633afd1e99782be5f3aa52b571d
Precious Adigwe <padigwe@charlotte.edu>
2025-10-20 20:33:56 -0400

    finished styling and formatting footer and contact components

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,22 @@
+import GithubIcon from "../assets/icons8-github.svg?react";
+import LinkedInIcon from "../assets/icons8-linked-in.svg?react";
+
 function Contact() {
   return (
-    <section>
-      <h3>Get in Touch</h3>
-      <p>I'd love to connect with you!</p>
-      <p>Email: padigwe06@gmail.com</p>
-      <div>
+    <section className="before:bg-accent relative z-0 mt-20 py-10 text-center text-white before:absolute before:top-1/2 before:left-1/2 before:-z-10 before:h-full before:w-screen before:-translate-x-1/2 before:-translate-y-1/2">
+      <h3 className="mb-10 text-7xl tracking-wider uppercase">Get in Touch</h3>
+      <p className="font-barlow-semi text-2xl text-white">
+        I'd love to connect with you!
+      </p>
+      <p className="font-barlow-semi text-2xl text-white">
+        Email: padigwe06@gmail.com
+      </p>
+      <div className="mt-10 flex justify-center gap-10">
         <a href="https://github.com/pradigwe/" target="_blank">
-          GitHub
+          <GithubIcon className="bg-neutral2 rounded-full p-2"></GithubIcon>
         </a>
         <a href="https://www.linkedin.com/in/pradigwe/" target="_blank">
-          LinkedIn
+          <LinkedInIcon className="bg-neutral2 rounded-full p-2"></LinkedInIcon>
         </a>
       </div>
     </section>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,18 @@
 function Footer() {
   return (
-    <footer>
-      <p>Designed & Built by Precious Adigwe</p>
-      <p>
-        SVG icons from
-        <a href="https://icons8.com/" target="_blank">
+    <footer className="mt-10 text-center">
+      <p className="text-sm">Designed & Built by Precious Adigwe</p>
+      <p className="text-sm">
+        SVG icons from {" "}
+        <a
+          href="https://icons8.com/"
+          target="_blank"
+          className="text-neutral2 underline"
+        >
           Icons8
         </a>
-        and
-        <a href="https://www.svgrepo.com/">SVG Repo</a>
+        {" "}and{" "}
+        <a href="https://www.svgrepo.com/" className="text-neutral2 underline">SVG Repo</a>
       </p>
     </footer>
   );
